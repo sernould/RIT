0bcdccf93e8cfc168b7fc137ec59f0a48f405f6f
Samuel Szoniecky <samszon@gmail.com>
2022-05-13 17:37:44 +0200

    exemple algo

--- getData.js
+++ getData.js
@@ -1,11 +1,66 @@
+const props = ['genstory:affect','genstory:actant','genstory:evenement','genstory:lieu','genstory:object'];
+let arrElemStory = []; 
 d3.json("https://genstory.jardindesconnaissances.fr/files/original/0b5592ef87ccd96e00cc82eaf8ed5f54a407ec11.json").then((data) => {
     console.log(data);
+        //console.log(data);
+        props.forEach(p=>{
+            arrElemStory[p] = data.layers.filter(l=>l.class["o:term"]==p);
+            showElemStory(p);
+        })
+});
+function showElemStory(p){
+    //get alea ElemeStory
+    let idES = d3.randomInt(arrElemStory[p].length-1)();	
+    let rdmES = arrElemStory[p][idES];
+    switch (p) {
+        case 'genstory:lieu':
+            appendImage(rdmES);
+            break;
+        case 'genstory:actant':
+            appendImage(rdmES);
+            break;
+        case 'genstory:evenement':         
+            appendText(rdmES);
+            break;      
+    }
+}
 
-    /*
-    data.layers.forEach(l => {
-        console.log(l.name);
+function appendImage(es){
+    let c = es.class["o:label"];
+
+    d3.select('body').selectAll('.'+c)
+        .data([es])
+        .join(
+          enter => enter.append("img")
+              .attr("class", c)
+              .attr("src", d => d.source.thumbnail_display_urls.medium)
+              .on('click',changeStory),
+          update => update.attr("src", d => d.source.thumbnail_display_urls.medium),
+          exit => exit.remove()
+        );        
+}
+function changeStory(e,d){
+    //showElemStory(d.class["o:term"]);
+    props.forEach(p=>{
+        showElemStory(p);
     });
-    */
+}
+
+function appendText(es){
+    let c = es.class["o:label"];
+
+    d3.select('body').selectAll('.'+c)
+        .data([es])
+        .join(
+          enter => enter.append("div")
+              .attr("class", c)
+              .text(d => d.source['o:title']),
+          update => update.text(d => d.source["o:title"]),
+          exit => exit.remove()
+        );        
+}
+
+/*   
 
     let layout = d3.select('body').selectAll('div').data(data.layers).enter().append('div');
 
@@ -39,6 +94,4 @@ d3.json("https://genstory.jardindesconnaissances.fr/files/original/0b5592ef87ccd
         })
         .on('click',clickActant);
     }
-
-    //hasLieu
-});
+*/
